f54f601a1bfedb322bf2d57880b8c0aad2aeb95a
Håkon Nilsen <hnilsen@gmail.com>
2014-01-23 00:19:30 +0100

    Removed an unnecessary import

--- rs/src/main/java/no/array/android/rs/MainActivity.java
+++ rs/src/main/java/no/array/android/rs/MainActivity.java
@@ -8,7 +8,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
